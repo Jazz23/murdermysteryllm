9c68a7e96948fd75f11608037b9ff4ac26afb41c
Jazz23 <noreply>
2025-03-11 10:15:39 -0700

    Add C# project for demo

---
+++ demo/MurderMysteryLLM/MurderMysteryLLM/AIAgent.cs
@@ -0,0 +1,10 @@
+﻿using OpenAI.Chat;
+
+namespace MurderMysteryLLM;
+
+public class AIAgent
+{
+    private readonly ChatClient _chatClient = new("gpt-4o-mini", Environment.GetEnvironmentVariable("OPENAI_API_KEY"));
+    
+    public async Task<string> Test() => (await _chatClient.CompleteChatAsync("What is your name?")).Value.Content.First().Text;
+}
---
+++ demo/MurderMysteryLLM/MurderMysteryLLM/Program.cs
@@ -0,0 +1,8 @@
+﻿using dotenv.net;
+using MurderMysteryLLM;
+
+// Load openai api key from .env file at the root of the project
+DotEnv.Load(options: new DotEnvOptions(probeForEnv: true, probeLevelsToSearch: 4));
+
+var agent = new AIAgent();
+Console.WriteLine(await agent.Test());
