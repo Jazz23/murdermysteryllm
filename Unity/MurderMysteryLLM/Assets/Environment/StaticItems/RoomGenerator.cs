0de96885b3aad2dcb24138d8bfd0d5683428c2ab
timpt0261 <rchavez0215@gmail.com>
2025-04-09 16:07:19 -0700

    Room Gen Sycn to Nav Mesh
    - When rooms are created, so is nav mesh for appropriate room

--- Unity/MurderMysteryLLM/Assets/Environment/StaticItems/RoomGenerator.cs
+++ Unity/MurderMysteryLLM/Assets/Environment/StaticItems/RoomGenerator.cs
@@ -1,6 +1,12 @@
-using UnityEditor.Rendering.Universal;
 using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using NavMeshPlus.Components;
 
+using UnityEngine.AI;
+
+
+[ExecuteInEditMode]
 public class RoomGenerator : MonoBehaviour
 {
     [SerializeField]
@@ -17,36 +23,47 @@ public class RoomGenerator : MonoBehaviour
 
     private Dictionary<string, Room> roomCached = new Dictionary<string, Room>();
 
-    private void OnGUI() {
-         GUI.Box(new Rect(10, 10, 300, 90), "Room Generator");
-         if (GUI.Button(new Rect(20, 40, 280, 20), "Generate Room")) {
-             GenerateRoom();
-         }
-            if (GUI.Button(new Rect(20, 70, 280, 20), "Clear Room")) {
-                ClearRoom();
-            }
-        
-        
+    [SerializeField]
+    private bool clear = false;
+
+    [SerializeField]
+    private NavMeshSurface surface= null;
+
+    private void OnGUI()
+    {
+        GUI.Box(new Rect(10, 10, 300, 90), "Room Generator");
+        if (GUI.Button(new Rect(20, 40, 280, 20), "Generate Room"))
+        {
+            GenerateRoom();
+        }
+        if (GUI.Button(new Rect(20, 70, 280, 20), "Clear Room"))
+        {
+            ClearRoom();
+        }
+
+
     }
-
-    private void GenerateRoom() {
-
+    public void GenerateRoom() {
         for (int r = 0; r < ROW_ROOMS; r++) {
             for (int c = 0; c < COL_ROOMS; c++){
 				// create new room at (r,c) coordinate
                 Room room = Instantiate(roomPrefab, new Vector3(r * OFFSET, c * OFFSET, 0), Quaternion.identity).GetComponent<Room>();
                 
                 room.transform.parent = this.transform;
-                room.name = "Room_" + r + "_" + c;
+                room.name = $"Room_{r}_{c}";
                 roomCached[room.name] = room;                
             }
         }
+
+        // bake mesh 
+        surface.BuildNavMeshAsync();
     }
 
-    private void ClearRoom() {
-        foreach (var room in roomCached.Values) {
-            Destroy(room.gameObject);
-        }
+    public void ClearRoom() {
+       
+        foreach (var room in roomCached.Values) 
+            DestroyImmediate(room.gameObject);
+        
         roomCached.Clear();
     }
 
