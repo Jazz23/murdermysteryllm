3a9b459c9f72f7147a9a84a5e5bf89065432caba
timpt0261 <rchavez0215@gmail.com>
2025-05-13 16:14:33 -0700

    modified agent observer and added rigid body to agent prefab

--- Unity/MurderMysteryLLM/Assets/AgentObserver.cs
+++ Unity/MurderMysteryLLM/Assets/AgentObserver.cs
@@ -11,16 +11,26 @@ public class AgentObserver : MonoBehaviour
     private List<GameObject> _searchableObject;
     [SerializeField]
     private List<GameObject> _peersNearby;
+    
+    [SerializeField]
+    private string _currentLocation;
+    public string CurrentLocation { get; private set; }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log($"Is being within range {other.gameObject.name}");
-        // if(other.CompareTag("Agent")|| other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        if(other.CompareTag("Agent"))
+        {
+            _peersNearby.Add(other.gameObject);
+        }
+        else if(other.CompareTag("Searchable") || other.gameObject.layer == LayerMask.NameToLayer("Searchable"))
+        {
+            _searchableObject.Add(other.gameObject);
+        // }else if  (other.gameObject.layer == LayerMask.NameToLayer("Location"))
         // {
-        //     _peersNearby.Append(other.gameObject);
-        // }
-        // else if(other.CompareTag("Searchable") || other.gameObject.layer == LayerMask.NameToLayer("Searchable"))
-        // {
-        //     _searchableObject.Append(other.gameObject);
+        //     // Do something with the location
+        //     _currentLocation = other.gameObject.name;
+        //     Debug.Log($"{this.gameObject.name }'s current location is {_currentLocation}");
         // }
 
     }
@@ -28,14 +38,14 @@ public class AgentObserver : MonoBehaviour
     private void OnTriggerExit2D(Collider2D other)
     {
         Debug.Log($"Is out of within range {other.gameObject.name}");
-        // if (other.CompareTag("Agent") || other.gameObject.layer == LayerMask.NameToLayer("Player"))
-        // {
-        //     _peersNearby.Remove(other.gameObject);
-        // }
-        // else if (other.CompareTag("Searchable") || other.gameObject.layer == LayerMask.NameToLayer("Searchable"))
-        // {
-        //    _searchableObject.Remove(other.gameObject);
-        // }
+        if (other.CompareTag("Agent") || other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            _peersNearby.Remove(other.gameObject);
+        }
+        else if (other.CompareTag("Searchable") || other.gameObject.layer == LayerMask.NameToLayer("Searchable"))
+        {
+           _searchableObject.Remove(other.gameObject);
+        }
     }
 
 
